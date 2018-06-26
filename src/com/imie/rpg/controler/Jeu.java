1527cd88e94844abeb5fcd6eccc8be99fa9d3d12
Bombalidos <bombalidos@hotmail.fr>
2018-06-26 16:11:05 +0200

    Debut d’implementation du controller

--- src/com/imie/rpg/controler/Jeu.java
+++ src/com/imie/rpg/controler/Jeu.java
@@ -1,14 +1,28 @@
 package com.imie.rpg.controler;
 
+import java.util.List;
+
+import com.imie.rpg.model.arme.Arme;
+import com.imie.rpg.model.armure.Armure;
+import com.imie.rpg.model.personnage.Barbare;
+import com.imie.rpg.model.personnage.Personnage;
 import com.imie.rpg.model.personnage.hero.IHero;
 import com.imie.rpg.model.personnage.mob.IMob;
 
 public class Jeu {
 	
-	public Jeu() {
-		
-	}
+	private static final int NBHEROS	= 7;
+	private static final int NBMOBS		= 5;
 	
+	private List<IPersonnage> personnages;
+	private List<Arme> armes;
+	private List<Armure> armures;
+	
+	public Jeu() {}
+	
+	public void nouvellePartie() {
+		this.nouvellePartie(NBHEROS, NBMOBS);
+	}
 	public void nouvellePartie(int nbHeros, int nbMobs) {
 		
 	}
@@ -17,16 +31,77 @@ public class Jeu {
 		
 	}
 	
-	public void combat(IHero attaquant, IMob adversaire) {
+	public void combat(IHero attaquant, IMob adversaire) {		
+		if ( ((Personnage) attaquant).getPointsAction() > 0 ) {
+			((Personnage) attaquant).equiper();
+		}
+		else
+			return;
 		
+		while ( ((Personnage) attaquant).getPointsAction() > 0 ) {
+			if ( ((Personnage) attaquant).getPointsAction() >= ((Personnage) attaquant).getArme().getPointsAction() ) {
+				taper(attaquant, adversaire, ((Personnage) attaquant).getArme());
+			}
+			
+			if ( attaquant instanceof Barbare
+					&& ((Personnage) attaquant).getPointsAction() > 0
+					&& ((Personnage) attaquant).getPointsAction() >= ((Barbare) attaquant).getArme2().getPointsAction())
+			{
+				taper(attaquant, adversaire, ((Barbare) attaquant).getArme2());
+			}
+		}
 	}
 	
 	public boolean hasCombattants() {
-		return false;
+		boolean result = false;
+		int nbHerosRestants	= 0;
+		int nbMobsRestants	= 0;
+		
+		for (IPersonnage perso : personnages) {
+			if ( perso instanceof IHero )
+				nbHerosRestants++;
+			else if ( perso instanceof IMob )
+				nbMobsRestants++;
+		}
+		
+		if ( nbHerosRestants > 0 && nbMobsRestants > 0 )
+			result = true;
+		
+		return result;
 	}
 	
 	public void afficherVainqueur() {
+		//Dernier restant en vie
+		IPersonnage vainqueur = personnages.get(0);
 		
+		StringBuilder txt = new StringBuilder();
+		
+		txt.append("Le dernier survivant et grand vainqueur est un ");
+		txt.append("… .\n");
+		
+		txt.append("Il est équipé d’une arme avec ").append(((Personnage) vainqueur).getArme().getValeurAttaque());
+		txt.append(" points d’attaque, et d’une armure avec ").append(((Personnage) vainqueur).getArmure().getValeurDefense());
+		txt.append(" points de défense. Il lui reste ");
+		txt.append(((Personnage) vainqueur).getPointsDeVie()).append(" points de vie.");
+		
+		System.out.println(txt.toString());
+	}
+	
+	private void taper (IHero attaquant, IMob adversaire, Arme arme) {
+		int degats = arme.getValeurAttaque()
+						- ((Personnage) adversaire).getArmure().getValeurDefense();
+		if ( degats < 0 )
+			degats = 0;
+		
+		((Personnage) adversaire).setPointsDeVie(
+				((Personnage) adversaire).getPointsDeVie()
+				- degats
+				);
+		
+		((Personnage) attaquant).setPointsAction(
+				((Personnage) attaquant).getPointsAction()
+				- arme.getPointsAction()
+				);
 	}
 
 }
